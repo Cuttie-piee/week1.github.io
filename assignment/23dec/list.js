24e47c8d58032212207e0179f31d7e3ba50019e8
Cuttie-piee <94856283+Cuttie-piee@users.noreply.github.com>
2022-01-01 14:55:38 +0530

    Add files via upload

---
+++ assignment/23dec/list.js
@@ -0,0 +1,42 @@
+var g_class = {
+  'name': "Computer Science Engineering",
+  'instructor': {
+    'name': "Priya Kumari",
+    'subject': "Artificial Intelligence"
+  },
+  'students': [{
+      'name': 'Prasad',
+      'marks': 86,
+    },
+    {
+      'name': 'Neha',
+      'marks': 85,
+    },
+    {
+      'name': 'Dhiraj',
+      'marks': 70,
+    },
+    {
+      'name': 'Keertan',
+      'marks': 82,
+    },
+    {
+     'name': 'Kavya',
+     'marks': 90,
+    },
+    {
+     'name': 'Nilima',
+     'marks': 95,
+    },
+    {
+     'name': 'Deepak',
+     'marks': 90,
+    }
+  ],
+};
+console.log(g_class.instructor.name);
+console.log(g_class['instructor']['subject']);
+console.log(g_class.students[0].name);
+console.log(g_class.students[4].name);
+console.log(g_class.students[5].name);
+console.log(g_class.students[6].name);
