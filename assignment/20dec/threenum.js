74e6ae31debad2b97f2a069afa4716dc5b9cf23d
Cuttie-piee <94856283+Cuttie-piee@users.noreply.github.com>
2022-01-01 17:26:29 +0530

    Add files via upload

---
+++ assignment/20dec/threenum.js
@@ -0,0 +1,7 @@
+alert("Check Greatest of all three numbers")
+var n1=parseInt(3);
+var n2=parseInt(5);
+var n3=parseInt(1);
+var max=Math.max(Math.max(n1,n2),n3)
+alert("The Greatest Number is  "+max)
+console.log(max);
