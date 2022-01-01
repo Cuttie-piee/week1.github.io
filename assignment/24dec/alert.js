266ef7edb45ceec998e4793bffdf2bab4d7aec39
Cuttie-piee <94856283+Cuttie-piee@users.noreply.github.com>
2022-01-01 18:44:40 +0530

    Add files via upload

---
+++ assignment/24dec/alert.js
@@ -0,0 +1,12 @@
+alert("The Factorial of a number")
+var n,n1,f;
+f=1
+var n=parseInt(prompt("Enter the number"));
+n1=n;
+while(n>0)
+{
+  f=f*n;
+  n--;
+}
+alert("Factorial of " +n1+ " is " +f);
+
